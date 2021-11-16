6bb9eda3f8464adfac9d94f78282848d21c4eba3
xalivan <xalivan@ukr.net>
2021-11-16 12:31:16 +0200

    1.2 COMMIT

--- src/main/java/com/example/test/Comits.java
+++ src/main/java/com/example/test/Comits.java
@@ -3,5 +3,6 @@ package com.example.test;
 public class Comits {
     public static void main(String[] args) {
         System.out.println("1 comment");
+        System.out.println("2 comment");
     }
 }
