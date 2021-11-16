7b407d9cfa94229b062826aa6dd28099801dfd1e
xalivan <xalivan@ukr.net>
2021-11-16 13:14:07 +0200

    2.5 COMMIT

--- src/main/java/com/example/test/Comits.java
+++ src/main/java/com/example/test/Comits.java
@@ -6,5 +6,6 @@ public class Comits {
         System.out.println("2 comment");
         System.out.println("3 comment");
         System.out.println("4 comment");
+        System.out.println("5 comment");
     }
 }
