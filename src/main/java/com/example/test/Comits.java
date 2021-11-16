76114d7777ac68f4d6af6d18dd0c80d0aa5d086a
xalivan <xalivan@ukr.net>
2021-11-16 12:31:33 +0200

    1.3 COMMIT

--- src/main/java/com/example/test/Comits.java
+++ src/main/java/com/example/test/Comits.java
@@ -4,5 +4,6 @@ public class Comits {
     public static void main(String[] args) {
         System.out.println("1 comment");
         System.out.println("2 comment");
+        System.out.println("3 comment");
     }
 }
