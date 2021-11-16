214106b27b84a744f50e93b1a0764dfa2c466492
xalivan <50083834+xalivan@users.noreply.github.com>
2021-11-16 13:09:24 +0200

    1 b (#1)
    * 1.1 COMMIT
    
    * 1.2 COMMIT
    
    * 1.3 COMMIT

---
+++ src/main/java/com/example/test/Comits.java
@@ -0,0 +1,9 @@
+package com.example.test;
+
+public class Comits {
+    public static void main(String[] args) {
+        System.out.println("1 comment");
+        System.out.println("2 comment");
+        System.out.println("3 comment");
+    }
+}
